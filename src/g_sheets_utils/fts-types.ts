83cacc914232ed4cf6da7d18cedc5583a97a717d
lironh <lironenk@gmail.com>
2021-01-15 18:38:38 +0200

    feat(*): *

---
+++ src/g_sheets_utils/fts-types.ts
@@ -0,0 +1,6 @@
+export type CbObj = { (auth: any): void; (arg0: any): void; };
+
+export interface Sheets { spreadsheets: { values: { get: (arg0: { spreadsheetId: any; range: any; }, arg1: (err: any, res: any) => void | Promise<unknown>) => void; }; }; }
+export interface Creds { installed: { client_secret: any; client_id: any; redirect_uris: any; }; }
+
+export interface OAuth2Client { generateAuthUrl: (arg0: { access_type: string; scope: string[]; }) => any; getToken: (arg0: any, arg1: (err: any, token: any) => void) => void; setCredentials: (arg0: any) => void; }
---
+++ src/g_sheets_utils/g-sheets.utils.ts
@@ -0,0 +1,27 @@
+
+// * Get and store new token after prompting for user authorization, and then
+// * execute the given callback with the authorized OAuth2 client.
+import {CbObj, OAuth2Client} from "./fts-types";
+import {promisify} from "util";
+const fs = require('fs');
+const readline = require('readline');
+const writeToFile = promisify(fs.writeFile);
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+export const TOKEN_PATH = 'src/private/token.json';
+
+export function getNewToken(oAuth2Client: OAuth2Client, callback: CbObj) {
+    const authUrl = oAuth2Client.generateAuthUrl({ access_type: 'offline', scope: SCOPES });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+    rl.question('Enter the code from that page here: ', (code: any) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) return console.error('Error while trying to retrieve access token', err);
+            oAuth2Client.setCredentials(token);
+            // Store the token to disk for later program executions
+            writeToFile(TOKEN_PATH, JSON.stringify(token))
+                .then(() => console.log('Token stored to', TOKEN_PATH))
+            callback(oAuth2Client);
+        });
+    });
+}
