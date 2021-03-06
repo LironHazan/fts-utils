1e3b6ce92a4f25698c554f923458bae057ab9f3f
lironh <lironenk@gmail.com>
2021-03-06 13:12:16 +0200

    feat(*): cli

--- fts-cli/src/commands/start.ts
+++ fts-cli/src/commands/start.ts
@@ -1,11 +1,11 @@
 import { Command } from '@oclif/command';
 import * as inquirer from 'inquirer';
 import { FLAGS, QUESTIONS } from '../tasks/start/consts';
-import { exportTask, importTask } from '../tasks/start/tasks';
+import { exportTask, importTask, optimizeSVG } from '../tasks/start/tasks';
 import { Action, None } from '../fts.types';
 
 export class MasterCmd extends Command {
-  static description = 'Initiates an Import or an Export task.' + '("./bin/run start -n=<name>")';
+  static description = 'Initiates a Task.' + '("./bin/run start -n=<name>")';
   static flags = FLAGS;
   static args = [{ name: 'file' }];
 
@@ -24,19 +24,24 @@ export class MasterCmd extends Command {
     }
 
     const action: Action = flags.action;
+
     if (!action) {
       const responses: any = await inquirer.prompt(QUESTIONS);
-      execTasks(responses.action);
+      const filepath = 'src/tasks/start/icon.svg';
+      const result = await execTasks(responses.action, filepath);
+      console.log((result as any)?.data ?? result);
     }
   }
 }
 
-function execTasks<T>(response: Action): T | None {
+async function execTasks<T>(response: Action, filepath: string): Promise<{ data: string } | T | None> {
   switch (response) {
     case 'export':
       return exportTask();
     case 'import':
       return importTask();
+    case 'opt_svg':
+      return await optimizeSVG(filepath);
     default:
       console.log('Nothing');
   }
